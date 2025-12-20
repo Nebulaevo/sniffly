74b2cc4fa45ab2e50168e0224d4d258ccd7366b0
Nebulaevo <didier.martin.dev@gmail.com>
2025-12-20 12:20:51 +0100

    added type prettifier and branded types to type utils

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -2,6 +2,19 @@
 /** Utility type representing key/value pair dictionnary with string keys */
 type Dict_T<EntriesType> = { [key:string]: EntriesType }
 
+/** Utility type allowing to unpack nested types */
+type Prettify_T<T> = {
+    [K in keyof T]: T[K]
+} & {}
+
+/** Utility type allowing to create branded types */
+declare const brand: unique symbol
+type Brand_T<
+    T, Brand_T extends string
+> = T & { [brand]: Brand_T }
+
 export type {
-    Dict_T
+    Dict_T,
+    Prettify_T,
+    Brand_T
 }
